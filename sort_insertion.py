0bbb89b0b8f9fcf7a5a924a1ef11236df48751e9
Zay Latt <zay_latt@me.com>
2025-04-07 12:10:56 +0100

    Improved two sorting algo's.

--- sort_insertion.py
+++ sort_insertion.py
@@ -1,11 +1,8 @@
 def insertion_sort(list_a):
-    index_length = range(1, len(list_a))
+    index_length = range(len(list_a))
     for i in index_length:
-        value_to_sort = list_a[i]
-
-        while list_a[i-1] > value_to_sort and i > 0:
+        while list_a[i-1] > list_a[i] and i > 0:
             list_a[i], list_a[i-1] = list_a[i-1], list_a[i]
             i = i-1
-
     return list_a
 print(insertion_sort([2,4,1,3,5,6,9,8,7]))
