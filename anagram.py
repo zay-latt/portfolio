443f52701b19626e45319c72edd82bc5cc7de4f7
Zay Latt <zay_latt@me.com>
2025-08-13 00:26:36 +0100

    Update

--- anagram.py
+++ anagram.py
@@ -1,6 +1,10 @@
 def is_anagram(s1, s2):
     s1 = s1.lower().replace(" ", "")
     s2 = s2.lower().replace(" ", "")
+
+    if len(s1) != len(s2):
+        return False
+    
     return sorted(s1) == sorted(s2)
 
 s1 = input("Enter first word: ")
